6d95a54f9126cd7ef706b19420cf88d8beb92cd1
Unagi-zoso <Unagi.zoso@gmail.com>
2024-12-04 11:56:11 +0900

    Feat 인증 컨텍스트 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext.jsx';
 import Header from './components/Header.jsx';
+import Login from './pages/Login.jsx'
 
 function App() {
   return (
-    <Router>
-      <ConditionalHeader />
-      <Routes></Routes>
-    </Router>
+    <AuthProvider>
+        <Router>
+        <ConditionalHeader />
+        <Routes>
+            <Route path='/login' element={<Login />}/>
+        </Routes>
+        </Router>
+    </AuthProvider>
   );
 }
 
