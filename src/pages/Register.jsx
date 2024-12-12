6e4293f7c35ed5d10fb04c5b63cdaf6d753c613b
Unagi-zoso <Unagi.zoso@gmail.com>
2024-12-13 00:12:54 +0900

    Refactor 회원가입 컴포넌트에 공용 컴포넌트 적용

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import '../stylesheets/pages/register.css';
-import userDefaultProfile from '../assets/user_default_profile.svg';
+import classNames from 'classnames';
+import InputField from '../components/ui/InputField';
+import SubmitButton from '../components/ui/SubmitButton';
+import ProfileImageInput from '../components/ui/ProfileImageInput'
+import styles from './Register.module.css';
 import { API_BASE_URL } from '../constants/api';
 
 export default function Register() {
@@ -152,131 +155,67 @@ export default function Register() {
         alert(result.message || '회원가입에 실패했습니다.');
       }
     } catch (error) {
-      console.error('회원가입 중 오류 발생:', error);
       alert('회원가입 중 오류가 발생했습니다.');
     }
   };
 
   return (
-    <div className="register">
-      <h1 className="register-logo-str">회원가입</h1>
-      <form className="register-form" onSubmit={handleSubmit}>
-        <div className="input-box-image">
-          <label htmlFor="profileImage">프로필 사진</label>
-          <div className="image-placeholder-wrapper">
-            <input
-              type="file"
-              id="profileImage"
-              name="profileImage"
-              accept="image/*"
-              onChange={handleProfileImageChange}
-            />
-            <div
-              className="image-placeholder"
-              onClick={() => document.getElementById('profileImage').click()}
-            >
-              {imagePreview ? (
-                <img
-                  id="imagePreview"
-                  className="image-preview"
-                  src={imagePreview}
-                  alt="Image Preview"
-                />
-              ) : (
-                <img
-                  className="placeholder-plus"
-                  src={userDefaultProfile}
-                  alt="plus"
-                />
-              )}
-            </div>
-          </div>
-        </div>
-
-        <div className="input-box">
-          <label htmlFor="email">이메일 *</label>
-          <input
-            type="text"
-            name="email"
-            placeholder="이메일을 입력하세요"
-            value={formData.email}
-            onChange={handleEmailChange}
-          />
-          <span
-            className="help-text"
-            style={{ visibility: errors.email ? 'visible' : 'hidden' }}
-          >
-            {errors.email}
-          </span>
-        </div>
-
-        <div className="input-box">
-          <label htmlFor="password">비밀번호 *</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="비밀번호를 입력하세요"
-            value={formData.password}
-            onChange={handlePasswordChange}
-          />
-          <span
-            className="help-text"
-            style={{ visibility: errors.password ? 'visible' : 'hidden' }}
-          >
-            {errors.password}
-          </span>
-        </div>
-
-        <div className="input-box">
-          <label htmlFor="passwordChecker">비밀번호 확인 *</label>
-          <input
-            type="password"
-            name="passwordChecker"
-            placeholder="비밀번호를 한 번 더 입력하세요"
-            value={formData.passwordChecker}
-            onChange={handlePasswordCheckerChange}
-          />
-          <span
-            className="help-text"
-            style={{
-              visibility: errors.passwordChecker ? 'visible' : 'hidden',
-            }}
-          >
-            {errors.passwordChecker}
-          </span>
-        </div>
-
-        <div className="input-box">
-          <label htmlFor="nickname">닉네임 *</label>
-          <input
-            type="text"
-            name="nickname"
-            placeholder="닉네임을 입력하세요"
-            value={formData.nickname}
-            onChange={handleNicknameChange}
-          />
-          <span
-            className={`help-text ${nicknameStatus === 'success' ? 'success' : ''}`}
-            style={{ visibility: 'visible' }}
-          >
-            {nicknameMessage}
-          </span>
-        </div>
-
-        <div className="btn-box">
-          <button
-            className="register-btn btn-submit"
-            type="submit"
-            disabled={
-              Object.values(errors).some(error => error) ||
-              !formData.email ||
-              !formData.password ||
-              nicknameStatus !== 'success'
+    <div className={styles.register}>
+      <h1 className={styles.registerLogoStr}>회원가입</h1>
+      <form className={styles.registerForm} onSubmit={handleSubmit}>
+        <ProfileImageInput
+          label="프로필 사진"
+          name="profileImage"
+          onChange={handleProfileImageChange}
+          preview={imagePreview}
+        />
+        <InputField
+          label="이메일 *"
+          name="email"
+          value={formData.email}
+          onChange={handleEmailChange}
+          placeholder="이메일을 입력하세요"
+          error={errors.email}
+        />
+        <InputField
+          label="비밀번호 *"
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handlePasswordChange}
+          placeholder="비밀번호를 입력하세요"
+          error={errors.password}
+        />
+        <InputField
+          label="비밀번호 확인 *"
+          name="passwordChecker"
+          type="password"
+          value={formData.passwordChecker}
+          onChange={handlePasswordCheckerChange}
+          placeholder="비밀번호를 한 번 더 입력하세요"
+          error={errors.passwordChecker}
+        />
+        <InputField
+          label="닉네임 *"
+          name="nickname"
+          value={formData.nickname}
+          onChange={handleNicknameChange}
+          placeholder="닉네임을 입력하세요"
+          error={nicknameMessage}
+          errorClassName={nicknameStatus === 'success'}
+        />
+        <div className={styles.btnBox}>
+          <SubmitButton
+            isValid={
+              !Object.values(errors).some(error => error) &&
+              formData.email &&
+              formData.password &&
+              nicknameStatus === 'success'
             }
-          >
-            회원가입
-          </button>
-          <Link className="login-redirect-btn" to="/login">
+            label="회원가입"
+            className={classNames(styles.btnSubmit, styles.registerBtn) }
+          />
+          <Link className={styles.loginRedirectBtn} to="/login">
             로그인 하러가기
           </Link>
         </div>
