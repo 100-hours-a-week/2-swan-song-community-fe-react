ac6274629d7e0c1330ea963b82615e04788bfa11
Unagi-zoso <Unagi.zoso@gmail.com>
2024-12-12 23:58:02 +0900

    Feat 버튼 공용 컴포넌트 작성

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = ({ isValid = true, label, onClick, className, type="button" }) => {
+  return (
+      <button
+        className={`${styles.defaultBtn} ${className || ""}`}
+        type={type}
+        disabled={!isValid}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+  );
+};
+
+export default Button;
