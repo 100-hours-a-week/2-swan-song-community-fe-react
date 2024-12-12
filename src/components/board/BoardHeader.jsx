9b67fcc8033188543604e422f3172f844b16a471
Unagi-zoso <Unagi.zoso@gmail.com>
2024-12-13 00:22:23 +0900

    Feat BoardHeader 컴포넌트 작성

---
+++ src/components/board/BoardHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./BoardHeader.module.css";
+
+const BoardHeader = () => {
+  return (
+    <div className={styles.boardHeader}>
+      <div>안녕하세요,</div>
+      <div>
+        아무 말 대잔치<span className={styles.textBold}> 게시판</span>입니다.
+      </div>
+    </div>
+  );
+};
+
+export default BoardHeader;
