b6b2d419fbaa13675219886c314a594d940953d5
Unagi-zoso <Unagi.zoso@gmail.com>
2024-12-17 04:20:13 +0900

    Refactor prettier 포매팅 적용

--- src/pages/UserPasswordModify.jsx
+++ src/pages/UserPasswordModify.jsx
@@ -22,16 +22,16 @@ const UserPasswordModify = () => {
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
     return isValidLength &&
-    hasNumber &&
-    hasUpperCase &&
-    hasLowerCase &&
-    hasSpecialChar
-        ? ''
-        : '* 비밀번호는 8자 이상 20자 이하이며, 특수문자, 영어, 숫자를 각각 하나 이상 포함해야 합니다.';
+      hasNumber &&
+      hasUpperCase &&
+      hasLowerCase &&
+      hasSpecialChar
+      ? ''
+      : '* 비밀번호는 8자 이상 20자 이하이며, 특수문자, 영어, 숫자를 각각 하나 이상 포함해야 합니다.';
   };
 
   const validatePasswordCheck = (password, passwordChecker) =>
-      password === passwordChecker ? '' : '* 비밀번호가 일치하지 않습니다.';
+    password === passwordChecker ? '' : '* 비밀번호가 일치하지 않습니다.';
 
   const handlePasswordChange = e => {
     const password = e.target.value;
@@ -74,49 +74,49 @@ const UserPasswordModify = () => {
     } catch (error) {
       console.error('비밀번호 수정 중 오류가 발생했습니다:', error);
     }
-  }
-    return (
-        <section className={styles.userPasswordModify}>
-          <h1 className={styles.headerStr}>비밀번호 수정</h1>
-          <form className={styles.userPasswordModifyForm}>
-            <InputField
-                label="새 비밀번호"
-                name="newPassword"
-                type="password"
-                value={newPassword}
-                onChange={handlePasswordChange}
-                placeholder="새 비밀번호를 입력하세요"
-                error={newPasswordMessage}
-                errorClassName={newPasswordStatus === 'success'}
-            />
-            <InputField
-                label="새 비밀번호 확인"
-                name="passwordCheck"
-                type="password"
-                value={passwordCheck}
-                onChange={handlePasswordCheckChange}
-                placeholder="새 비밀번호를 다시 입력하세요"
-                error={passwordCheckMessage}
-                errorClassName={passwordCheckStatus === 'success'}
-            />
-            <div>
-              <SubmitButton
-                  label={'수정하기'}
-                  className={styles.modifyButton}
-                  onClick={handleModify}
-                  disabled={
-                      !newPassword ||
-                      !passwordCheck ||
-                      newPasswordStatus === 'error' ||
-                      passwordCheckStatus === 'error'
-                  }
-              >
-                수정하기
-              </SubmitButton>
-            </div>
-          </form>
-        </section>
-    );
   };
+  return (
+    <section className={styles.userPasswordModify}>
+      <h1 className={styles.headerStr}>비밀번호 수정</h1>
+      <form className={styles.userPasswordModifyForm}>
+        <InputField
+          label="새 비밀번호"
+          name="newPassword"
+          type="password"
+          value={newPassword}
+          onChange={handlePasswordChange}
+          placeholder="새 비밀번호를 입력하세요"
+          error={newPasswordMessage}
+          errorClassName={newPasswordStatus === 'success'}
+        />
+        <InputField
+          label="새 비밀번호 확인"
+          name="passwordCheck"
+          type="password"
+          value={passwordCheck}
+          onChange={handlePasswordCheckChange}
+          placeholder="새 비밀번호를 다시 입력하세요"
+          error={passwordCheckMessage}
+          errorClassName={passwordCheckStatus === 'success'}
+        />
+        <div>
+          <SubmitButton
+            label={'수정하기'}
+            className={styles.modifyButton}
+            onClick={handleModify}
+            disabled={
+              !newPassword ||
+              !passwordCheck ||
+              newPasswordStatus === 'error' ||
+              passwordCheckStatus === 'error'
+            }
+          >
+            수정하기
+          </SubmitButton>
+        </div>
+      </form>
+    </section>
+  );
+};
 
-    export default UserPasswordModify;
+export default UserPasswordModify;
