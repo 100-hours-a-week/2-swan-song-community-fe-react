eb487b6d48fd0bf91feafeedaeda7441f8426c27
Unagi-zoso <Unagi.zoso@gmail.com>
2024-12-18 23:36:01 +0900

    Chore 서버 URL 명시

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8181/api/v1';
-export const IMAGE_BASE_URL = 'http://localhost:8181/';
+export const API_BASE_URL = 'http://54.180.104.196:8181/api/v1';
+export const IMAGE_BASE_URL = 'http://54.180.104.196:8181/';
